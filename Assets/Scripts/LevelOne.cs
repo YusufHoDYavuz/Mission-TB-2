7f053bee4a21b62703a2240d0b8735e859b5b082
Veloncys <veloncys@gmail.com>
2022-08-22 22:58:25 +0300

    StartMenu UI (first) (not working btns)

--- Assets/Scripts/LevelOne.cs
+++ Assets/Scripts/LevelOne.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +10,4 @@ public class LevelOne : MonoBehaviour
     {
         Destroy(infoUI,9);
     }
-
-   
 }
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public void startGame()
+    {
+        Debug.Log("oyun başladı");
+    }
+
+    public void levelMenu()
+    {
+        Debug.Log("level menüye girildi");
+    }
+
+    public void settingsMenu()
+    {
+        Debug.Log("ayaralara girildi");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("oyundan çıkış yapıldı");
+    }
+}
